683b20dd7cf0ea976ea9e85626f5d05f0b2625ce
MrSahilShaikh <shaikhsahils2000@gmail.com>
2024-05-13 11:56:19 +0530

    tailwind update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        danger: "#f7737375",
+        primary: "#60a5fa",
+      },
+    },
+  },
+  plugins: [],
+};
+
